75e0b22afa24c3e2ce4a41c49d6341cceca4390b
vandegn <vandegn@miamioh.edu>
2026-03-14 10:09:17 -0400

    tightening cors allowed stuff

--- backend/app/main.py
+++ backend/app/main.py
@@ -18,7 +18,7 @@ app.add_middleware(
     allow_origins=os.getenv("CORS_ORIGINS", "http://localhost:3000").split(","),
     allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"],
+    allow_headers=["Content-Type", "Authorization"],
 )
 
 # Include routers
